d8d30d861257cc8c36c13d09f64986d5d5147255
LoopFruits <Kensworldc@gmail.com>
2022-04-05 16:00:39 -0400

    complete assignmnet

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,7 +3,9 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
+    # we could do this as well 
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
@@ -11,5 +13,12 @@ class CheesesController < ApplicationController
     cheese = Cheese.find_by(id: params[:id])
     render json: cheese
   end
+
+  # we can include the summary from our instance method in the JSON response as well. 
+  # it would look like this 
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  # end
 
 end
